82ac82c66e1e868992f6db72b2e8b51bc862b094
Romain-Barbe <oramnixe91@gmail.com>
2023-02-06 15:36:09 +0100

    AdministrationController routes made

--- backend/src/main/java/fr/esipe/way2go/controller/AdministrationController.java
+++ backend/src/main/java/fr/esipe/way2go/controller/AdministrationController.java
@@ -1,10 +1,37 @@
 package fr.esipe.way2go.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/administration")
 public class AdministrationController {
+    @GetMapping
+    //Page d'administration
+    public String administrationPage() {
+        return "Page d'administration";
+    }
 
+    @DeleteMapping("/user/remove/{userId}")
+    //Supprime un utilisateur
+    private String removeUser(@PathVariable long userId) {
+        return "Supprime un utilisateur";
+    }
+
+    @PutMapping("/invite/send/{inviteId}")
+    //Envoie une invitation
+    private String sendInvite(@PathVariable long inviteId) {
+        return "Envoie une invitation";
+    }
+
+    @PutMapping("/invite/resend/{inviteId}")
+    //Re-envoie une invitation
+    private String resendInvite(@PathVariable long inviteId) {
+        return "Re-envoie une invitation";
+    }
+
+    @DeleteMapping("/invite/remove/{inviteId}")
+    //Supprime une invitation
+    private String removeInvite(@PathVariable long inviteId) {
+        return "Supprime une invitation";
+    }
 }
