88015aae9499ee62b59069cb010c7dc1cdc2a3a9
Romain-Barbe <oramnixe91@gmail.com>
2023-02-06 15:19:25 +0100

    administrationController empty (test the commit)

---
+++ backend/src/main/java/fr/esipe/way2go/controller/AdministrationController.java
@@ -0,0 +1,10 @@
+package fr.esipe.way2go.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/administration")
+public class AdministrationController {
+
+}
