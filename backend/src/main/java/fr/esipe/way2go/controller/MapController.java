0245b8cde56bf8f0b28894db52c68be2c842207d
Romain-Barbe <oramnixe91@gmail.com>
2023-02-06 16:35:14 +0100

    ForgottenPasswordController and MapController routes added

--- backend/src/main/java/fr/esipe/way2go/controller/MapController.java
+++ backend/src/main/java/fr/esipe/way2go/controller/MapController.java
@@ -1,9 +1,6 @@
 package fr.esipe.way2go.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/map")
