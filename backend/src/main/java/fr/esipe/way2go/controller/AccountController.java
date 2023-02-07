4ae7812b7ddafa16955166322e56653b6e7a08bd
Romain-Barbe <oramnixe91@gmail.com>
2023-02-07 10:34:14 +0100

    Updated AccountController routes

--- backend/src/main/java/fr/esipe/way2go/controller/AccountController.java
+++ backend/src/main/java/fr/esipe/way2go/controller/AccountController.java
@@ -3,7 +3,7 @@ package fr.esipe.way2go.controller;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/createAccount")
+@RequestMapping("/api/createAccount")
 public class AccountController {
     @GetMapping
     //Page de création de compte
@@ -15,5 +15,17 @@ public class AccountController {
     //Crée le compte utilisateur
     public String createAccount() {
         return "Création du compte";
+    }
+
+    @PostMapping("/lostPassword")
+    //Demande de réinitialisation de mot de passe
+    public String lostPassword() {
+        return "Demande de réinitialisation de mot de passe pour l'email";
+    }
+
+    @PutMapping("/updateUser")
+    //Changement de mot de passe
+    public String changedPassword() {
+        return "Changement de mot de passe";
     }
 }
