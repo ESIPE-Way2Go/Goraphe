16965c3c5f51ca4c872a3c36af2c8fdce4d7708a
Vincent <vincent-dao@outlook.com>
2023-02-07 14:45:59 +0100

    Add map > can zoom / move

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -16,6 +16,11 @@ const routes = [
         component: () => import('@/views/LoginPage.vue'),
         meta: {requiresAuth: false, layout: 'blank'}
     },
+    {   path: '/map',
+        name: "map",
+        component: () => import('@/views/Home-map.vue'),
+        meta: {requiresAuth: false, layout: 'content'}
+    },
     {   path: '/',
         name: 'home',
         component: () => import('@/views/HomePage.vue'),
