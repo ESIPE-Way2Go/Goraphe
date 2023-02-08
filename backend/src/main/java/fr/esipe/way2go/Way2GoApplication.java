d210f8bc2c272b7b6e5e53e7c216a78518bdb1a6
Vincent <vincent-dao@outlook.com>
2023-02-08 08:06:15 +0100

    add can exec python and retrieve std output

--- backend/src/main/java/fr/esipe/way2go/Way2GoApplication.java
+++ backend/src/main/java/fr/esipe/way2go/Way2GoApplication.java
@@ -6,23 +6,29 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.nio.file.Path;
 
-@SpringBootApplication
+//@SpringBootApplication
 public class Way2GoApplication {
     public static void main(String[] args) {
-        SpringApplication.run(Way2GoApplication.class, args);
-        var command = "python3 /test.py";
+//        SpringApplication.run(Way2GoApplication.class, args);
+        var p = System.getProperty("user.dir") + System.getProperty("file.separator")+ "scripts" + System.getProperty("file.separator") + "test.py";
+        //var p = new ClassPathResource("test.py").getPath();
+        System.out.println(p);
+        var builder = new ProcessBuilder("python3", Path.of(p).toString(), "10");
+        builder.redirectErrorStream(true);
         try {
-            var process = Runtime.getRuntime().exec(command);
-            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
-            String line;
-            while ((line = reader.readLine()) != null) {
-                System.out.println(line);
-            }
-        } catch (IOException e) {
+            Process process = builder.start();
+            var res = new String(process.getInputStream().readAllBytes());
+            int exitCode = process.waitFor();
+//            System.out.println("result = " + res);
+//            System.out.println("exitCode = " + exitCode);
+
+            System.out.println("result"+res);
+
+        } catch (IOException | InterruptedException e) {
             throw new RuntimeException(e);
         }
 
     }
-
 }
