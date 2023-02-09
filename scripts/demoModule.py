b79441b933fd10d9ab7735e5b406b3bd02e49378
rubens <rubens-dc-conde@hotmail.fr>
2023-02-09 17:52:37 +0100

    - simple demo python script that prints a Geojson of the edges of a graph to the console

---
+++ scripts/demoModule.py
@@ -0,0 +1,20 @@
+import argparse
+import osmnx as ox
+
+parser = argparse.ArgumentParser()
+
+# generation distance for the graph
+parser.add_argument("--dist", type=int, required=True)
+# middle coordinates of the two points
+parser.add_argument("--coords", type=lambda x: tuple(map(float, x.split(','))), required=True)
+# Parse the command-line arguments
+args = parser.parse_args()
+
+dist = args.dist
+coords = args.coords
+
+#create the graph
+G = ox.graph_from_point(coords, dist)
+
+edges = ox.graph_to_gdfs(G,nodes=False,edges=True)
+print(edges.to_json())
