9963a84579f95cff368d4da8d6ea597a5cbaa3e8
Jeremy <jere.valade@gmail.com>
2023-02-09 10:30:13 +0100

    zdd request simulation

---
+++ backend/src/main/java/fr/esipe/way2go/dto/simulation/request/SimulationRequest.java
@@ -0,0 +1,16 @@
+package fr.esipe.way2go.dto.simulation.request;
+
+public class SimulationRequest {
+    /**
+     * Id of the simulation
+     */
+    private Long id;
+
+    /**
+     * Get the id of the simulation
+     * @return
+     */
+    public Long getId() {
+        return id;
+    }
+}
