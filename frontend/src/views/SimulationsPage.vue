14ac0744007d7c15386b981c9210b0d2ce0e720c
Jeremy <jere.valade@gmail.com>
2023-02-15 18:10:10 +0100

    add popup simulation

--- frontend/src/views/SimulationsPage.vue
+++ frontend/src/views/SimulationsPage.vue
@@ -1,95 +1,68 @@
 <template>
     <v-container class="bg-surface-variant">
         <v-row no-gutters>
-            <v-col v-for="simulation in simulations" :key=simulation.id cols="12" sm="4">
-                <v-card class="mr-4 mb-4">
+            <v-col v-for="simulation in simulations" :key=simulation.id cols="12" sm="3" lg="2">
+                <v-card class="ma-2">
                     <v-img class="align-end text-white" height="200" :src=imageTest cover></v-img>
                     <v-card-title class="pt-4">
                         {{ simulation.title }}
                     </v-card-title>
+                    <v-card-subtitle> {{ simulation.date }}</v-card-subtitle>
                     <v-card-actions class="justify-space-between">
                         <v-btn color="orange" @click="goSimulation(simulation.id)">
                             Détails
                         </v-btn>
-                        <v-btn icon="mdi-trash-can-outline"></v-btn>
+                        <v-btn icon="mdi-trash-can-outline" @click="deleteSimulationDialog(simulation)"></v-btn>
                     </v-card-actions>
                 </v-card>
             </v-col>
-        </v-row>       
+        </v-row>
     </v-container>
 
+    <v-dialog v-model="deleteDialog" max-width="600">
+        <v-card>
+            <v-toolbar color="primary">
+                <v-toolbar-title>Suppression de la simulation <b>{{ simulationTitle }}</b> </v-toolbar-title>
+            </v-toolbar>
 
-    <div class="mx-auto w-75">
-        <!-- <v-sheet :class="model" class="mx-auto mt-8" elevation="12" height="256" width="100%">
-            <div class="d-flex">
-                <v-chip class="ma-2 align-center" color="green" text-color="white">
-                    En cours
-                </v-chip>
-                <div v-for="simulation in simulationsInLoad" :key="simulation.title">
-                    <v-card class="mx-auto" max-width="400">
-                        <v-img class="align-end text-white" height="200" src="../assets/test.png" cover>
-                            <v-card-title>{{ simulation.title }}</v-card-title>
-                        </v-img>
+            <v-card-text>
+                Attention ! Vous êtes sur le point de supprimer définitivement la simulation <b>{{
+                    simulationTitle
+                }}</b>.
+                Cette action est irréversible et entraînera la suppression de toutes les données de
+                cette simulation, y compris tous les résultats et les analyses associées.
+            </v-card-text>
 
-                        <v-card-subtitle class="pt-4">
-                            Number 10
-                        </v-card-subtitle>
-
-                        <v-card-text>
-                            <div>Whitehaven Beach</div>
-
-                            <div>Whitsunday Island, Whitsunday Islands</div>
-                        </v-card-text>
-
-                        <v-card-actions>
-                            <v-btn color="orange">
-                                Share
-                            </v-btn>
-
-                            <v-btn color="orange">
-                                Explore
-                            </v-btn>
-                        </v-card-actions>
-                    </v-card>
-                </div>
-            </div>
-        </v-sheet>-->
-        <!--<v-sheet :class="model" class="mx-auto mt-8" elevation="12" height="256" width="100%">   </v-sheet> 
-         <v-chip class="ma-2 align-center" color="green" text-color="white">
-                Terminé
-            </v-chip>
-        
-        
-
-        <v-sheet class="d-flex justify-center flex-wrap bg-surface-variant" width="100%">
-            <div v-for="simulation in simulations" :key="simulation.title" width="100%">
-                <v-card class="mr-4 mb-4" width="20%">
-                    <v-img class="align-end text-white" height="200" :src=imageTest cover></v-img>
-                    <v-card-title class="pt-4">
-                        {{ simulation.title }}
-                    </v-card-title>
-                    <v-card-actions class="justify-space-between">
-                        <v-btn color="orange" @click="goSimulation(simulation.id)">
-                            Détails
-                        </v-btn>
-                        <v-btn icon="mdi-trash-can-outline"></v-btn>
-                    </v-card-actions>
-                </v-card>
-            </div>
-        </v-sheet>-->
-
-    </div>
+            <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="green" @click="deleteDialog = false">Annuler</v-btn>
+                <v-btn color="danger" variant="text" @click="deleteSimulation(simulationId)">
+                    Supprimer
+                </v-btn>
+            </v-card-actions>
+        </v-card>
+    </v-dialog>
 </template>
 
 <script>
 import authHeader from "@/services/auth-header";
+import { useToast } from "vue-toastification";
 
 export default {
+    setup() {
+        const toast = useToast();
+
+        return { toast }
+    },
     data() {
         return {
             simulationsInLoad: [],
             simulations: [],
-            imageTest: require('@/assets/test.png')
+            imageTest: require('@/assets/test.png'),
+            deleteDialog: false,
+            simulationId: -1,
+            simulationTitle: "",
+
         }
     },
 
@@ -102,7 +75,7 @@ export default {
                 .then(response => response.json())
                 .then(data => {
                     data.forEach(element => {
-                        var elt = { id: element['id'], title: element['title'], date: element['beginDate'] };
+                        var elt = { id: element['id'], title: element['title'], date: this.getFormatDate(element['beginDate']) };
                         if (element['endDate'] === null)
                             this.simulationsInLoad.push(elt)
                         else
@@ -112,6 +85,34 @@ export default {
         },
         goSimulation(id) {
             this.$router.push({ name: 'simulation', params: { id: id } });
+        },
+        getFormatDate(date) {
+            var d = new Date(date);
+            const year = d.getFullYear();
+            const month = d.getMonth() + 1;
+            const day = d.getDate();
+            const formattedDate = `${day.toString().padStart(2, '0')}/${month.toString().padStart(2, '0')}/${year}`;
+            return formattedDate
+        },
+        deleteSimulationDialog(simulation) {
+            this.simulationId = simulation.id
+            this.simulationTitle = simulation.title
+            this.deleteDialog = true
+        },
+        deleteSimulation(id) {
+            this.deleteDialog = false
+            fetch("/api/simulation/" + id, {
+                method: "DELETE",
+                headers: authHeader(),
+            }).then(response => response.json())
+                .then(data => {
+                    if (data === true) {
+                        this.toast.success(`Suppression de la simulation réussi`)
+                        this.getSimuations()
+                    } else {
+                        this.toast.error(`Suppression de la simulation pas réussi`)
+                    }
+                });
         }
     },
     mounted() {
