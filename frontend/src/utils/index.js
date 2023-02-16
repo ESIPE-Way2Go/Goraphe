132d3e246bb1a04787e1ecf64f19264e74adef54
Maxime <44499833+MaksDUT@users.noreply.github.com>
2023-02-16 14:30:04 +0100

    Revert "Barre de navigation (#26)"
    This reverts commit 14307c1bba7282c6113a50a818c89f0f8635f02f.

--- frontend/src/utils/index.js
+++ frontend/src/utils/index.js
@@ -1,4 +1,4 @@
-import {getCurrentInstance, reactive, toRefs, watch} from "vue";
+import {getCurrentInstance, reactive, toRefs, watch } from "vue";
 
 export const useRouter = () => {
     const vm = getCurrentInstance().proxy
@@ -6,37 +6,32 @@ export const useRouter = () => {
         route: vm.$route,
     })
 
-    watch(() => vm.$route, r => {
-        state.route = r
-    },)
+    watch(
+        () => vm.$route,
+        r => {
+            state.route = r
+        },
+    )
 
     return {...toRefs(state), router: vm.$router}
 }
 
 export const _ = null
 
-/*
- * This method allows to filter access to only the people authorized to access it
- * Using can("admin") give access only to the administrators
- * Using can("user") restrict access to the guests
- * Using can() with any other String will return false
- */
 export const can = (role) => {
-    if (role === null) {
+    if(role===null){
         return false;
     }
     const user = JSON.parse(localStorage.getItem('user'));
-    if (user === null) return false;
+    if(user===null) return false;
     const role_user = user.roles[0]
-    if (role_user === null) {
+    if(role_user === null){
         return false;
     }
     switch (role_user) {
         case "ROLE_ADMIN" :
-            return true // The admins can access all the things
-        case "ROLE_USER" :
-            return role === "user"
+            return role === "admin"
         default :
-            return false
+            return role === "user"
     }
 }
