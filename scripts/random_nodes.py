44a429b21541c6375e181635c5201b76c4882b10
Romain Barbé <62951218+Romain-Barbe@users.noreply.github.com>
2023-02-21 10:01:21 +0100

    Random points python (#36)
    * First draft of random points, can be upgraded.
    
    * First draft of random points, can be upgraded.
    
    * Version of random_nodes.py used to merge with the other scripts (see if we can upgrade it later).

--- scripts/random_nodes.py
+++ scripts/random_nodes.py
@@ -1,23 +1,37 @@
+import argparse
 
-def random_nodes():
-    #esipe
-    random_nodes = [4805558775, 2411236822, 2411236813, 10004464578, 250836394, 4805558771]
+import osmnx as ox
+import networkx as nx
+import random
 
-    #espagne
-    #random_nodes = [289455025, 988941419, 2093962711, 3717146176, 4448101620, 4561569022]
-    # colour and size list to plot random nodes on the network graph
-    # nc = ['b' if osmid in random_nodes
-    #       else 'grey' for osmid in graph_proj.nodes()]
-    #
-    # ns = [100 if osmid in random_nodes
-    #       else 1 for osmid in graph_proj.nodes()]
-    #
-    # fig, ax = ox.plot_graph(graph_proj,figsize=(30,30), bgcolor='w',
-    #                       show=False, save=True, close=True,
-    #                      filepath=r"C:\Users\silvi\Desktop\PostDoc_PANOPTIS\Complex Network_RI\new_script\Set_37\images\Graph_random_nodes.png",
-    #                     dpi=300, node_color=nc, node_edgecolor='grey',
-    #                    node_size=ns, node_zorder=3)
 
-    #########################################
-    print("\nRandom_nodes : \n"+str(type(random_nodes))+"\n"+str(random_nodes))
-    return random_nodes
+def random_nodes(G, x1, y1, x2, y2):
+    # define the number of random points that will be used (this number will be around the first chosen node AND around
+    # the second chosen node ; so there will be random points equals to 2 times the number chosen below in total)
+    n_random_points = 10
+    # define a distance around each one of the 2 chosen nodes to create random points
+    distance = 0.0125
+
+    # create the random points list, consisting of 2 "n_random_points" size lists in which will be stored our random
+    # points ("n_random_points" in the first list and "n_random_points" in the second list ; used to separate our points
+    # that will be around the 2 different chosen nodes)
+    random_points = [[0 for _ in range(n_random_points)] for _ in range(2)]
+
+    # define 2 x "n_random_points" at random positions in the graph (10 around each chosen node at a
+    # distance <= "distance")
+    for i in range(n_random_points):
+        rx1 = random.uniform(-distance, distance)
+        ry1 = random.uniform(-distance, distance)
+        rx2 = random.uniform(-distance, distance)
+        ry2 = random.uniform(-distance, distance)
+        # find the first random point
+        first_random_node = ox.distance.nearest_nodes(G, x1 + rx1, y1 + ry1)
+        # find the second random point
+        second_random_node = ox.distance.nearest_nodes(G, x2 + rx2, y2 + ry2)
+        # add the first random point
+        random_points[0][i] = first_random_node
+        # add the second random point
+        random_points[1][i] = second_random_node
+
+    return random_points
+
