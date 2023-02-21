d267502784e8375e7c89574941b25c211e3b297a
Vincent <vincent-dao@outlook.com>
2023-02-21 16:39:04 +0100

    All checks done from backend for new simulation, returns "bad request" in case of a bad request done from postman or the similar

--- backend/src/main/java/fr/esipe/way2go/dto/simulation/request/SimulationRequest.java
+++ backend/src/main/java/fr/esipe/way2go/dto/simulation/request/SimulationRequest.java
@@ -6,13 +6,13 @@ import java.util.List;
 public class SimulationRequest {
 
     String name;
-    double [] start;
-    double [] end;
+    double[] start;
+    double[] end;
     int distance;
     String desc;
     String script;
     List<String> roadTypes;
-    double [] center;
+    double[] center;
 
     public String getName() {
         return name;
@@ -90,5 +90,13 @@ public class SimulationRequest {
                 ", roadTypes=" + roadTypes +
                 ", center=" + Arrays.toString(center) +
                 '}';
+    }
+
+    public boolean checkBounds() {
+        double[][] worldBounds = {{-180.0, -90.0}, {180.0, 90.0}}; // Define the world bounds as a 2D array
+        // Check if each coordinate is within the world bounds
+        return Arrays.stream(start).allMatch(coord -> coord >= worldBounds[0][0] && coord <= worldBounds[1][0] && coord >= worldBounds[0][1] && coord <= worldBounds[1][1])
+                && Arrays.stream(end).allMatch(coord -> coord >= worldBounds[0][0] && coord <= worldBounds[1][0] && coord >= worldBounds[0][1] && coord <= worldBounds[1][1])
+                && Arrays.stream(center).allMatch(coord -> coord >= worldBounds[0][0] && coord <= worldBounds[1][0] && coord >= worldBounds[0][1] && coord <= worldBounds[1][1]);
     }
 }
