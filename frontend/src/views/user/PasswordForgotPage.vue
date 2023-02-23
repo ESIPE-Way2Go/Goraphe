b857591cafd2978db6ddb3714ffde41dc366fdc3
Romain-Barbe <oramnixe91@gmail.com>
2023-02-23 11:40:37 +0100

    PasswordForgotPage.vue front made correctly ; see how to do when the user zoom.

--- frontend/src/views/user/PasswordForgotPage.vue
+++ frontend/src/views/user/PasswordForgotPage.vue
@@ -1,17 +1,24 @@
 <template>
   <div class="d-flex align-center fill-height bg-image">
-    <v-card class="mx-auto pa-6 bg-white rounded-xl outline" height="65%" style="border-color: blue" variant="outlined"
-            width="40%">
-      <v-img :src=imageGoraphe class="mx-auto" cover width="40%"></v-img>
+    <v-card class="mx-auto pa-6 bg-white rounded-xl outline" max-height="60%" style="border-color: blue" variant="outlined"
+            width="30%">
+      <v-img :src=imageGoraphe class="mx-auto" cover width="50%"></v-img>
       <v-card-title class="text-center ma-5 text-blue-accent-3 font-weight-bold">
         Mot de passe oublié
       </v-card-title>
       <v-card-text>
         <v-form v-model="isFormValid" fast-fail @submit.prevent="sendForgotPassword">
           <v-text-field v-model="email" :rules="emailRules" label="Mail" required variant="outlined"></v-text-field>
-          <v-btn :disabled="!isFormValid" class="mt-2 rounded-3 mx-auto d-table bg-blue-darken-1" type="submit">
-            Envoyer
-          </v-btn>
+          <div class="d-flex justify-content-between">
+            <v-btn class="mt-2 rounded-3 bg-grey-darken-3" size="large"
+                   @click="this.$router.push({ name: 'login'})">
+              Retour
+            </v-btn>
+            <v-btn :disabled="!isFormValid" class="mt-2 rounded-3 bg-blue-darken-1" size="large"
+                   type="submit">
+              Envoyer
+            </v-btn>
+          </div>
         </v-form>
       </v-card-text>
     </v-card>
