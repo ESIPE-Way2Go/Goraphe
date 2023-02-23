2e9b872a0cefabb7219d0476ab78a8d14be35bba
Romain-Barbe <oramnixe91@gmail.com>
2023-02-23 11:17:20 +0100

    PasswordForgotPage.vue front made correctly.

--- frontend/src/views/user/PasswordForgotPage.vue
+++ frontend/src/views/user/PasswordForgotPage.vue
@@ -1,54 +1,76 @@
 <template>
-    <v-form fast-fail @submit.prevent="sendForgotPassword" v-model="isFormValid">
-        <v-text-field v-model="email" :rules="emailRules" label="Email" required></v-text-field>
-        <v-btn type="submit" block class="mt-2" :disabled="!isFormValid">Récupérer le mot de passe</v-btn>
-    </v-form>
+  <div class="d-flex align-center fill-height bg-image">
+    <v-card class="mx-auto pa-6 bg-white rounded-xl outline" height="65%" style="border-color: blue" variant="outlined"
+            width="40%">
+      <v-img :src=imageGoraphe class="mx-auto" cover width="40%"></v-img>
+      <v-card-title class="text-center ma-5 text-blue-accent-3 font-weight-bold">
+        Mot de passe oublié
+      </v-card-title>
+      <v-card-text>
+        <v-form v-model="isFormValid" fast-fail @submit.prevent="sendForgotPassword">
+          <v-text-field v-model="email" :rules="emailRules" label="Mail" required variant="outlined"></v-text-field>
+          <v-btn :disabled="!isFormValid" class="mt-2 rounded-3 mx-auto d-table bg-blue-darken-1" type="submit">
+            Envoyer
+          </v-btn>
+        </v-form>
+      </v-card-text>
+    </v-card>
+  </div>
 </template>
 
 <script>
-import { useToast } from "vue-toastification";
+import {useToast} from "vue-toastification";
 
 export default {
-    setup() {
-        const toast = useToast();
-        return { toast }
+  setup() {
+    const toast = useToast();
+    return {toast}
+  },
+  data: () => ({
+    valid: false,
+    email: '',
+    emailRules: [
+      value => {
+        if (value) return true
+        return 'Un mail est requis'
+      },
+      value => {
+        if (/^\w+([\\.-]?\w+)*@\w+([\\.-]?\w+)*(\.\w{2,})+$/.test(value)) return true
+        return 'Le mail doit être valide'
+      },
+    ],
+    isFormValid: false,
+    imageGoraphe: require('@/assets/Goraphe_small.png'),
+  }),
+  computed: {
+    formIsValid() {
+      return this.$refs.form.validate()
     },
-    data: () => ({
-        valid: false,
-        email: '',
-        emailRules: [
-            value => {
-                if (value) return true
-                return 'mail est requis'
-            },
-            value => {
-                if (/^\w+([\\.-]?\w+)*@\w+([\\.-]?\w+)*(\.\w{2,})+$/.test(value)) return true
-                return 'Mail doit être valide'
-            },
-        ],
-        isFormValid: false
-    }),
-    computed: {
-        formIsValid() {
-            return this.$refs.form.validate()
-        },
-    },
-    methods: {
-        sendForgotPassword() {
-            fetch("/api/administration/forgetPassword/" + this.email, {
-                method: "GET",
-                headers: { "Content-Type": "application/json" },
+  },
+  methods: {
+    sendForgotPassword() {
+      fetch("/api/administration/forgetPassword/" + this.email, {
+        method: "GET",
+        headers: {"Content-Type": "application/json"},
+      })
+          .then(response => {
+            if (response.ok) {
+              this.toast.success("Mail envoyé")
+              this.$router.push({name: 'login'})
+            }
+            response.json().then(data => {
+              this.toast.error(data['message'])
             })
-                .then(response => {
-                    if (response.ok) {
-                        this.toast.success("Mail envoyé")
-                        this.$router.push({ name: 'login' })
-                    }
-                    response.json().then(data => {
-                        this.toast.error(data['message'])
-                    })
-                })
-        },
-    }
+          })
+    },
+  }
 }
 </script>
+
+<style>
+.bg-image {
+  background-image: url('@/assets/roads_backgroung_img.svg');
+  background-size: cover;
+  background-position: center center;
+}
+</style>
