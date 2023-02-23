4aa7a90d3d19e248b41f97dd03390e4a1e3dc44e
Romain-Barbe <oramnixe91@gmail.com>
2023-02-23 13:45:36 +0100

    Changed the CreateAccountPage.vue front style according to the CDCG.

--- frontend/src/views/user/CreateAccountPage.vue
+++ frontend/src/views/user/CreateAccountPage.vue
@@ -1,28 +1,29 @@
 <template>
-  <div class="d-flex align-center fill-height">
-    <v-card width="25%" class="mx-auto pa-6 bg-white" style="border-color: blue" variant="outlined" rounded-lg>
-      <v-img class="align-end text-white" :src=imageGoraphe cover></v-img>
-      <v-card-title class="text-center" style="color: blue">
+  <div class="d-flex align-center fill-height bg-image">
+    <v-card class="mx-auto pa-6 bg-white rounded-xl" max-height="700px" style="border-color: blue" variant="outlined"
+            width="500px">
+      <v-img :src=imageGoraphe class="mx-auto" cover width="50%"></v-img>
+      <v-card-title class="text-center ma-5 text-blue-accent-3 font-weight-bold" max>
         Création de compte
       </v-card-title>
       <v-card-text>
-        <v-form fast-fail @submit.prevent="createAccount" v-model="isFormValid">
-          <v-text-field variant="outlined" v-model="mail" label="Mail" readonly></v-text-field>
-          <v-text-field variant="outlined" v-model="form.username" label="Pseudo"
-                        :rules="usernameRules"></v-text-field>
-
-          <v-text-field variant="outlined" v-model="form.password"
-                        :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'" :rules="passwordRules"
-                        :type="show1 ? 'text' : 'password'" label="Mot de passe "
-                        hint="Au moins 8 caractères avec au moins une majuscule, une minuscule et un chifffre" counter
+        <v-form v-model="isFormValid" fast-fail @submit.prevent="createAccount">
+          <v-text-field v-model="form.mail" class="ma-2" label="Mail" readonly variant="outlined"></v-text-field>
+          <v-text-field v-model="form.username" :rules="usernameRules" class="ma-2" label="Pseudonyme"
+                        variant="outlined"></v-text-field>
+          <v-text-field v-model="form.password" :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'" :rules="passwordRules"
+                        :type="show1 ? 'text' : 'password'" class="ma-2"
+                        hint="Au moins 8 caractères, une majuscule, une minuscule et un chifffre"
+                        label="Mot de passe " variant="outlined"
                         @click:append="show1 = !show1"></v-text-field>
-
-          <v-text-field variant="outlined" v-model="confirmPassword"
-                        :append-icon="show2 ? 'mdi-eye' : 'mdi-eye-off'" :rules="confirmPasswordRules"
-                        :type="show2 ? 'text' : 'password'" label="Confirmation de mot de passe"
+          <v-text-field v-model="confirmPassword" :append-icon="show2 ? 'mdi-eye' : 'mdi-eye-off'"
+                        :rules="confirmPasswordRules"
+                        :type="show2 ? 'text' : 'password'" class="ma-2"
+                        label="Confirmation du mot de passe" variant="outlined"
                         @click:append="show2 = !show2"></v-text-field>
-
-          <v-btn type="submit" color=primary block class="mt-2" :disabled="!isFormValid" rounded>Valider</v-btn>
+          <v-btn :disabled="!isFormValid" class="mt-2 rounded-3 bg-blue-darken-1 mx-auto d-table" size="large" type="submit">
+            Valider
+          </v-btn>
         </v-form>
       </v-card-text>
     </v-card>
@@ -45,20 +46,20 @@ export default {
       usernameRules: [
         value => {
           if (value !== '') return true
-          return 'Le pseudo ne doit pas être vide'
+          return 'Ce champ ne doit pas être vide'
         },
       ],
       passwordRules: [
         value => {
           if ((/^(?=.*[A-Z])(?=.*[a-z])(?=.*\d).{8,}$/).test(value)) return true
-          return 'Le mot de passe doit avoir au moins huits caractères, une majuscule, une minuscule et un chiffre'
+          return 'Le mot de passe doit contenir au moins 8 caractères, une majuscule, une minuscule et un chiffre'
         },
       ],
       confirmPassword: '',
       confirmPasswordRules: [
         value => {
           if (value === this.form.password) return true
-          return "La confirmation du mot de passe n'est pas identique au mot de passe"
+          return 'Ce champ n\'est pas identique au champ "Mot de passe"'
         },
       ],
 
@@ -74,7 +75,7 @@ export default {
   },
   methods: {
     checkAutorization() {
-      var token = this.$route.params.token
+      let token = this.$route.params.token
       fetch("/api/administration/checkAccount/" + token, {
         method: "GET",
       }).then(response => response.json()).then(
@@ -107,3 +108,11 @@ export default {
   }
 }
 </script>
+
+<style>
+.bg-image {
+  background-image: url('@/assets/roads_backgroung_img.svg');
+  background-size: cover;
+  background-position: center center;
+}
+</style>
