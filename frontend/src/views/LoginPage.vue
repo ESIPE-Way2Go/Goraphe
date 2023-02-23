49c018e1b14fd8642ee8c586662c544fd5def8d8
Romain-Barbe <oramnixe91@gmail.com>
2023-02-23 11:15:24 +0100

    Clean LoginPage.vue.

--- frontend/src/views/LoginPage.vue
+++ frontend/src/views/LoginPage.vue
@@ -1,9 +1,7 @@
 <template>
   <div class="container-fluid">
     <div class="row no-gutter">
-
       <div class="col-md-6 d-none d-md-flex bg-image"></div>
-
       <div class="col-md-6">
         <div class="login d-flex align-items-center py-5">
           <div class="container">
@@ -13,46 +11,47 @@
                 <p class="text-muted mb-4">The road to success.</p>
                 <form @submit.prevent="handleLogin">
                   <div class="mb-3">
-                    <input id="inputEmail" type="text" v-model="form.username" placeholder="username" required=""
-                      autofocus="" class="form-control rounded-pill border-0 shadow-sm px-4" />
+                    <input id="inputEmail" v-model="form.username" autofocus=""
+                           class="form-control rounded-pill border-0 shadow-sm px-4" placeholder="Username"
+                           required="" type="text"/>
                   </div>
                   <div class="mb-3">
-                    <input id="inputPassword" type="password" v-model="form.password" placeholder="Password" required=""
-                      class="form-control rounded-pill border-0 shadow-sm px-4 text-primary" />
+                    <input id="inputPassword" v-model="form.password"
+                           class="form-control rounded-pill border-0 shadow-sm px-4 text-primary" placeholder="Password"
+                           required=""
+                           type="password"/>
                   </div>
                   <div class="text-center">
-                    <v-btn variant="text"  color="blue" @click="this.$router.push({ name: 'forgetPassword'})">
+                    <v-btn color="blue" variant="text" @click="this.$router.push({ name: 'forgetPassword'})">
                       Mot de passe oublié ?
                     </v-btn>
                   </div>
                   <div class="d-grid gap-2 mt-2">
-                    <button class="btn btn-primary btn-block text-uppercase mb-2 rounded-pill shadow-sm">Sign in</button>
+                    <button class="btn btn-primary btn-block text-uppercase mb-2 rounded-pill shadow-sm">Sign in
+                    </button>
                   </div>
-
                 </form>
               </div>
             </div>
           </div>
-
         </div>
       </div>
-
     </div>
   </div>
 </template>
 
 <script>
-import { useToast } from "vue-toastification";
+import {useToast} from "vue-toastification";
 
 export default {
   name: 'LoginPage',
   setup() {
     const toast = useToast();
-    return { toast }
+    return {toast}
   },
   data() {
     return {
-      form: { username: "", password: "" },
+      form: {username: "", password: ""},
       errors: {}
     }
   },
@@ -60,12 +59,12 @@ export default {
 
     handleLogin() {
       this.$store.dispatch("auth/login", this.form).then((response) => {
-        if (response.email) {
-          this.$router.push({ name: 'home' });
-          return;
-        }
-        this.toast.error(response.message)
-      },
+            if (response.email) {
+              this.$router.push({name: 'home'});
+              return;
+            }
+            this.toast.error(response.message)
+          },
       );
     },
   }
@@ -73,8 +72,7 @@ export default {
 </script>
 
 <style>
-.login,
-.image {
+.login {
   min-height: 100vh;
 }
 
