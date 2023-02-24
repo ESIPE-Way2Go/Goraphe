aadba2c7200bfdbb71ca5ecf16ca4cf460fc7e71
rubens <rubens-dc-conde@hotmail.fr>
2023-02-24 09:12:15 +0100

    - fix distance generation of random nodes and commented export of excel

--- scripts/random_nodes.py
+++ scripts/random_nodes.py
@@ -17,7 +17,7 @@ def setup_logger(name, log_file, level=logging.DEBUG):
 
     return logger
 
-def random_nodes(G_proj, G_not_proj, x1, y1, x2, y2,user ,sim):
+def random_nodes(G_proj, G_not_proj, x1, y1, x2, y2,user ,sim,dist):
     # Creation of logger
     os.makedirs("scripts/" + user, exist_ok=True)
     LOG_FILENAME = os.getcwd() + "/scripts/" + user + "/" + sim + "_2.log"
@@ -27,7 +27,7 @@ def random_nodes(G_proj, G_not_proj, x1, y1, x2, y2,user ,sim):
     # the second chosen node ; so there will be random points equals to 2 times the number chosen below in total)
     n_random_points = 3
     # define a distance around each one of the 2 chosen nodes to create random points
-    distance = 0.0125
+    distance = dist/1000000
 
     # create the random points list, consisting of 2 "n_random_points" size lists in which will be stored our random
     # points ("n_random_points" in the first list and "n_random_points" in the second list ; used to separate our points
@@ -63,7 +63,7 @@ def random_nodes(G_proj, G_not_proj, x1, y1, x2, y2,user ,sim):
     # create a list of colors to highlight the random points
     node_colors = ['blue' if node in random_points else 'gray' for node in G_proj.nodes()]
     # plot the graph with highlighted random points
-    #ox.plot_graph(G_proj,node_color=node_colors ,node_size=50 ,show=True)
+    ox.plot_graph(G_proj,node_color=node_colors ,node_size=50 ,show=True)
     logger.info("End of random_nodes")
     return random_points
 
