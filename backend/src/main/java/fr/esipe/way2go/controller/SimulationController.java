06091a9a3e41dd4172457c8019a8880bbec8b4cd
rubens <rubens-dc-conde@hotmail.fr>
2023-03-01 16:51:30 +0100

    - small fix

--- backend/src/main/java/fr/esipe/way2go/controller/SimulationController.java
+++ backend/src/main/java/fr/esipe/way2go/controller/SimulationController.java
@@ -69,19 +69,14 @@ public class SimulationController {
         var simulation = new SimulationEntity(simulationRequest.getName(), user, simulationRequest.getDesc(), simulationRequest.getDistance(),  simulationRequest.getScript(), simulationRequest.getRoadTypes());
         var simulationSave = simulationService.save(simulation);
         var midPoint= new MapController.Point(simulationRequest.getCenter());
-
-        simulationSave.setBeginDate(Calendar.getInstance());
-        simulationSave.setStatus(StatusSimulation.LOAD);
-        simulationService.save(simulationSave);
-        scriptPythonService.executeScript(user, simulationSave, midPoint, simulationRequest);
-        /*var thread = new Thread(() -> {
+        var thread = new Thread(() -> {
             simulationSave.setBeginDate(Calendar.getInstance());
             simulationSave.setStatus(StatusSimulation.LOAD);
             simulationService.save(simulationSave);
             scriptPythonService.executeScript(user, simulationSave, midPoint, simulationRequest);
         });
         thread.start();
-        THREAD_SIMULATIONS.putIfAbsent(simulationSave.getSimulationId(), thread);*/
+        THREAD_SIMULATIONS.putIfAbsent(simulationSave.getSimulationId(), thread);
         return new ResponseEntity<>(new SimulationIdResponse(simulationSave.getSimulationId()), HttpStatus.ACCEPTED);
     }
 
